abbea8375c1c495988ef60e49737f3d2a0cc1a80
Assiya Hammougua <hammouguaassiyagmail.com>
2025-03-04 23:07:08 +0000

    Description des modifications

--- app.js
+++ app.js
@@ -42,12 +42,18 @@ app.use(express.json());
 app.use('/api/users', authRoutes);  // Route pour la gestion des utilisateurs (inscription, connexion)
 
 console.log('Routes configurées pour /api/users');
+const mongoose = require('mongoose');
+require('dotenv').config();  // Charge les variables d'environnement
 
-// Connexion à MongoDB
-mongoose.connect(process.env.MONGO_URI , { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('✅ Connecté à MongoDB'))
-  .catch(err => console.log('❌ Erreur de connexion à MongoDB :', err));
+const mongoURI = process.env.MONGO_URI || 'mongodb://mongo:VRejDTZtIqhMNUnuJoAlEqYAeELHURki@shuttle.proxy.rlwy.net:28774/cv-app';
 
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('✅ Connecté à MongoDB');
+  })
+  .catch((err) => {
+    console.log('❌ Erreur de connexion à MongoDB :', err);
+  });
  
   
 // Middleware de logs pour les requêtes
